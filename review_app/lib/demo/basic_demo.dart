a915f9a557241c8f02d8fe6adf7189c7104d655d
海神 <179590100@qq.com>
2019-08-16 13:27:15 +0800

    基础部件练习

---
+++ review_app/lib/demo/basic_demo.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class BasicDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    
+    return Container(
+      color: Colors.grey[100],
+      // padding: ,
+      // height: ,
+      // decoration: ,
+      // child: ,
+    );
+  }
+}
+
+class RichTextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: "ninghao",
+        style: TextStyle(
+            color: Colors.deepPurpleAccent,
+            fontSize: 34.0,
+            fontStyle: FontStyle.italic),
+        children: [
+          TextSpan(
+            text: ".net",
+            style: TextStyle(fontSize: 17),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "君不见黄河之水天上来，奔流到海不复还。君不见高堂明镜悲白发，朝如青丝暮成雪。人生得意须尽欢，莫使金樽空对月。天生我材必有用，千金散尽还复来.烹羊宰牛且为乐，会须一饮三百杯。",
+      textAlign: TextAlign.center,
+      maxLines: 3,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+        fontSize: 15,
+      ),
+    );
+  }
+}
--- review_app/lib/main.dart
+++ review_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'demo/drawer_demo.dart';
 import 'demo/listview_demo.dart';
 import 'demo/bottom_navigation_bar_demo.dart';
+import 'demo/basic_demo.dart';
 
 void main() => runApp(App());
 
@@ -48,11 +49,7 @@ class Home extends StatelessWidget {
     );
     List<Widget> tabBarViews = [
       ListViewDemo(),
-      Icon(
-        Icons.change_history,
-        size: 128,
-        color: Colors.black12,
-      ),
+      BasicDemo(),
       Icon(
         Icons.directions_bike,
         size: 128,
